ee385f8b65f881df42ad8ef4277d8f8207c44480
asmagulo <asmagulo@L02R11S07s-iMac.local>
2024-11-08 18:28:50 +0500

    js__link

--- main/script.js
+++ main/script.js
@@ -1,6 +1,13 @@
 function toggleMenu() {
     const menu = document.getElementById('dropdown-menu');
     menu.classList.toggle('active');
-}
 
+    // Добавляем обработчик события для закрытия меню при клике на любую ссылку внутри него
+    const links = menu.querySelectorAll('.nav__link');
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active'); // Закрываем меню
+        });
+    });
+}
 
